c1f2e64eb387a2250354df6b8e04882b76bf48a0
madeiramcqueen <madeira@mcqueen.net>
2022-12-18 13:18:51 -0800

    Debug. Lab Part 1 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Add the Cat class & list and view function below the imports
 class Finch:  # Note that parens are optional if not inheriting from another class
@@ -10,12 +9,12 @@ class Finch:  # Note that parens are optional if not inheriting from another cla
 
 finches = [
     Finch('Fred', 'Speedy little thing', 3),
-    Finch('Marco', 'Tiny little guy', 0),
+    Finch('Marco', 'Tiny little Yellow Finch', 0),
 ]
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Finch Collector Home PAge!</h1>')
+  return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
