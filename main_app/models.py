decbde3b1a1e67b3304ed8df2ecca21888089193
madeiramcqueen <madeira@mcqueen.net>
2022-12-18 12:56:10 -0800

    Finches page complete

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
 from django.db import models
 
 # Create your models here.
+# Add the Cat class & list and view function below the imports
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add the Cat class & list and view function below the imports
+class Finch:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, description, age):
+    self.name = name
+    self.description = description
+    self.age = age
+
+finches = [
+    Finch('Fred', 'Speedy little thing', 3),
+    Finch('Marco', 'Tiny little guy', 0),
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Finch Collector Home PAge!</h1>')
 
 def about(request):
-    return HttpResponse('<h1> About Finches </h1>')
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {"finches": finches})
