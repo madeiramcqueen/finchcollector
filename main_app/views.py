2be314048cd04b91e42c3361f0207aa2187d4128
madeiramcqueen <madeira@mcqueen.net>
2022-12-19 13:23:51 -0800

    Clean up code and Part 2 complete

--- main_app/views.py
+++ main_app/views.py
@@ -2,28 +2,12 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Finch
 
-
-# Add the Cat class & list and view function below the imports
-# class Finch:  # Note that parens are optional if not inheriting from another class
-#   def __init__(self, name, description, age):
-#     self.name = name
-#     self.description = description
-#     self.age = age
-
-# finches = [
-#     Finch('Fred', 'Speedy little thing', 3),
-#     Finch('Marco', 'Tiny little Yellow Finch', 0),
-# ]
-
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
-
-# def finches_index(request):
-#     return render(request, 'finches/index.html', {"finches": finches})
 
 class FinchList(ListView):
   model = Finch
