37fefcd622d804a3f7c21d5457f623cd9186381e
madeiramcqueen <madeira@mcqueen.net>
2022-12-19 12:59:21 -0800

    Refactor view function to class base function and add data via python3 shell

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-# Add the Cat class & list and view function below the imports
+# Add the Finch class & list and view function below the imports
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
