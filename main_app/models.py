d166e1c36de30e7d3d80bdff4ab228e87875fc3b
madeiramcqueen <madeira@mcqueen.net>
2022-12-20 12:10:20 -0800

    Create feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,12 @@ from django.urls import reverse
 # Create your models here.
 # Add the Finch class & list and view function below the imports
 
+MEALS = (
+    ('E', 'Early Morning'),
+    ('M', 'Mid-day'),
+    ('E', 'Evening')
+)
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -15,4 +21,15 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse("detail", kwargs={"finch_id": self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices = MEALS,
+        default=MEALS[0][0]
+        )
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
     
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
