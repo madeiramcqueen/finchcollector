c455c8916de68f389306bb57fb614f09d56e5899
madeiramcqueen <madeira@mcqueen.net>
2022-12-20 11:31:18 -0800

    Add a finch form

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 from .models import Finch
 
 # Create your views here.
@@ -12,6 +13,10 @@ def about(request):
 class FinchList(ListView):
   model = Finch
   template_name = 'finches/index.html'
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
